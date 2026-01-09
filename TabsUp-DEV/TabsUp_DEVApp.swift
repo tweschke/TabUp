725a9e5ebc6bb1d6d42e522e42aeac159229e431
Thomas W <65774908+tweschke@users.noreply.github.com>
2026-01-09 11:21:13 +0000

    Initial Commit

---
+++ TabsUp-DEV/TabsUp_DEVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabsUp_DEVApp.swift
+//  TabsUp-DEV
+//
+//  Created by Thomas Weschke on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TabsUp_DEVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabsUp-DEVTests/TabsUp_DEVTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabsUp_DEVTests.swift
+//  TabsUp-DEVTests
+//
+//  Created by Thomas Weschke on 09/01/2026.
+//
+
+import Testing
+@testable import TabsUp_DEV
+
+struct TabsUp_DEVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
